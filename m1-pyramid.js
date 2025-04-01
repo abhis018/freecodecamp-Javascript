d50f96caa8f5ba304ee7a6cc787cd54542cd2979
abhis018 <76279317+abhis018@users.noreply.github.com>
2025-04-02 00:08:28 +0530

    changes the intendation of the file

--- m1-pyramid.js
+++ m1-pyramid.js
@@ -4,20 +4,20 @@ const rows = [];
 let inverted = false;
 function padRow(rowNumber, rowCount) {
     return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
-  }
+}
   
-  for (let i = 1; i <= count; i++) {
+for (let i = 1; i <= count; i++) {
     if (inverted) {
       rows.unshift(padRow(i, count));
     } else {
       rows.push(padRow(i, count));
     }
-  }
+}
   
-  let result = ""
+let result = ""
   
-  for (const row of rows) {
+for (const row of rows) {
     result = result + row + "\n";
-  }
+}
   
-  console.log(result);
+console.log(result);
