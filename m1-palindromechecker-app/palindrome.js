717657bf74c95f3fb0234b18990006517e29533e
abhis018 <76279317+abhis018@users.noreply.github.com>
2025-04-20 23:05:19 +0530

    building a palindrome checker app

---
+++ m1-palindromechecker-app/palindrome.js
@@ -0,0 +1,6 @@
+function alertFunc() {
+    if(document.getElementById("text-input").value == ""){
+      alert("Please input a value");
+    }
+  }
+  const res = document.getElementById("result");
