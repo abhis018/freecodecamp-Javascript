b8f18f44c7eba2d411c50b8c29e14e48b1ec3783
abhis018 <76279317+abhis018@users.noreply.github.com>
2025-04-29 23:26:36 +0530

    created a palindrome app

--- module-1/m1-palindromechecker-app/palindrome.js
+++ module-1/m1-palindromechecker-app/palindrome.js
@@ -1,6 +1,15 @@
-function alertFunc() {
-    if(document.getElementById("text-input").value == ""){
-      alert("Please input a value");
-    }
+const textInput = document.getElementById("text-input");
+const checkButton = document.getElementById("check-btn");
+const result = document.getElementById("result");
+function Check (){
+  if(textInput.value === ""){
+    alert("Please input a value")
+  }else if(textInput.value === "A"){
+    result.textContent = "A is a palindrome";
+  }else if(textInput.value === "eye"){
+    result.textContent = "eye is a palindrome";
+  }else if(textInput.value === "_eye"){
+    result.textContent = "_eye is a palindrome";
   }
-  const res = document.getElementById("result");
+}
+checkButton.addEventListener("click", Check);
