4169b01714fe7a8900787befeaa139df35e11473
abhis018 <76279317+abhis018@users.noreply.github.com>
2025-04-30 20:04:26 +0530

    added a function for checking a number is palindrome or not

--- module-1/m1-palindromechecker-app/palindrome.js
+++ module-1/m1-palindromechecker-app/palindrome.js
@@ -2,14 +2,14 @@ const textInput = document.getElementById("text-input");
 const checkButton = document.getElementById("check-btn");
 const result = document.getElementById("result");
 function Check (){
+  const reverseText = textInput.value.replace(/\s+/g, '').split("").reverse().join("");
+  console.log(reverseText);
   if(textInput.value === ""){
     alert("Please input a value")
-  }else if(textInput.value === "A"){
-    result.textContent = "A is a palindrome";
-  }else if(textInput.value === "eye"){
-    result.textContent = "eye is a palindrome";
-  }else if(textInput.value === "_eye"){
-    result.textContent = "_eye is a palindrome";
+  }else if(textInput.value === reverseText){
+    result.textContent = `${textInput.value} is a palindrome`;
+  }else {
+    result.textContent = `${textInput.value} is not a palindrome`;
   }
 }
 checkButton.addEventListener("click", Check);
