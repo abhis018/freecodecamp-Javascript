971df61cb5823435a9097426bde9ea47ff92294d
abhis018 <76279317+abhis018@users.noreply.github.com>
2025-05-01 00:46:33 +0530

    updated with the function part for the app

--- module-1/m1-palindromechecker-app/palindrome.js
+++ module-1/m1-palindromechecker-app/palindrome.js
@@ -2,11 +2,10 @@ const textInput = document.getElementById("text-input");
 const checkButton = document.getElementById("check-btn");
 const result = document.getElementById("result");
 function Check (){
-  const reverseText = textInput.value.replace(/\s+/g, '').split("").reverse().join("");
-  console.log(reverseText);
+  const reverseText = textInput.value.replace(/[^a-z0-9]/gi, '').toLowerCase().split("").reverse().join("");
   if(textInput.value === ""){
-    alert("Please input a value")
-  }else if(textInput.value === reverseText){
+    alert("Please input a value");
+  }else if(textInput.value.replace(/[^a-z0-9]/gi, '').toLowerCase() === reverseText){
     result.textContent = `${textInput.value} is a palindrome`;
   }else {
     result.textContent = `${textInput.value} is not a palindrome`;
