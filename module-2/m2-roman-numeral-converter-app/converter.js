abd872028364c1c0912b9d9db9c53b6d83ac8f4c
abhis018 <76279317+abhis018@users.noreply.github.com>
2025-05-04 02:10:32 +0530

    changes the bug in the app

--- module-2/m2-roman-numeral-converter-app/converter.js
+++ module-2/m2-roman-numeral-converter-app/converter.js
@@ -4,10 +4,10 @@ const output = document.getElementById("output");
 
 function Convert(num) {
   const numMap = {1:"I", 4:"IV", 5:"V",9:"IX", 10: "X",40:"XL",50:"L",90:"XC",100:"C",400:"CD",500:"D",900:"CM",1000:"M"};
-  let divisor = [1,4,5,9,10,40,50,90,100,400,500,900,100];
+  let divisor = [1,4,5,9,10,40,50,90,100,400,500,900,1000];
   let romanVal = '';
   if (num === "") {
-    alert("Please enter a valid number");
+    output.textContent = "Please enter a valid number";
   } else if (num <= 0) {
     output.textContent = `Please enter a number greater than or equal to 1`;
   } else if (num >= 4000) {
