cd9f38968180ddba6b5c7862072851cae7feaeb8
abhis018 <76279317+abhis018@users.noreply.github.com>
2025-05-12 22:49:47 +0530

    added comments to the js file

--- module-3/m3-spreadsheet-app/spreadsheet.js
+++ module-3/m3-spreadsheet-app/spreadsheet.js
@@ -1,11 +1,11 @@
-const range = (start, end) => Array();
+const range = (start, end) => Array(); // created an empty array inside the range function
 
 window.onload = () => {
-  const container = document.getElementById("container");
+  const container = document.getElementById("container"); //select an element(container) from the html by id
   const createLabel = (name) => {
-    const label = document.createElement("div");
-    label.className = "label";
-    label.textContent = name;
-    container.appendChild(label);
+    const label = document.createElement("div"); // created a div element and place it into the label variable
+    label.className = "label"; // added a classname(label) to that div
+    label.textContent = name; // set a text to that div 
+    container.appendChild(label); // insert that div inside the container element
   }
 }
