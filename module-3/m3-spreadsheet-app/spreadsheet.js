9b60556d31e8eb9d648f3f06c08824eab470bcf6
abhis018 <76279317+abhis018@users.noreply.github.com>
2025-05-13 23:09:36 +0530

    created a object called spreadsheetFunction to keep track records of all the function

--- module-3/m3-spreadsheet-app/spreadsheet.js
+++ module-3/m3-spreadsheet-app/spreadsheet.js
@@ -11,6 +11,12 @@ const median = nums => {
     : sorted[Math.ceil(middle)];
 }
 
+const spreadsheetFunctions = {
+  sum,
+  average,
+  median
+}
+
 const range = (start, end) => 
   Array(end - start + 1)        // create an array of appropriate length
     .fill(start)                // fill all elements with the starting value
