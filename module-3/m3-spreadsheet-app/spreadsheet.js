d14519d80560a0cb6de0acb99ab61c6ec628d2ee
abhis018 <76279317+abhis018@users.noreply.github.com>
2025-05-14 19:46:34 +0530

    created a function that matches the cell of the spreadsheet like A1:B4

--- module-3/m3-spreadsheet-app/spreadsheet.js
+++ module-3/m3-spreadsheet-app/spreadsheet.js
@@ -26,6 +26,17 @@ const charRange = (start, end) =>
   range(start.charCodeAt(0), end.charCodeAt(0))
   .map(code => String.fromCharCode(code));
 
+  const evalFormula = (x, cells) => {
+    const idToText = id => cells.find(cell => cell.id === id).value;
+    const rangeRegex = /([A-J])([1-9][0-9]?):([A-J])([1-9][0-9]?)/gi;
+    const rangeFromString = (num1, num2) => range(parseInt(num1), parseInt(num2));
+    const elemValue = num => character => idToText(character + num);
+    const addCharacters = character1 => character2 => num => charRange(character1, character2).map(elemValue(num));
+    const rangeExpanded = x.replace(rangeRegex, (_match, char1, num1, char2, num2) => rangeFromString(num1, num2).map(addCharacters(char1)(char2)));
+    const cellRegex = /[A-J][1-9][0-9]?/gi;
+    const cellExpanded = rangeExpanded.replace(cellRegex, match => idToText(match.toUpperCase()));
+  }
+
 window.onload = () => {
   const container = document.getElementById("container"); //select an element(container) from the html by id
   const createLabel = (name) => {
@@ -52,7 +63,7 @@ range(1, 99).forEach(number => {
 const update = event => {
   const element = event.target;
   const value = element.value.replace(/\s/g, "");
-  if (!value.includes(element.id)) {
+  if (!value.includes(element.id) && value.startsWith('=')) {
 
   }
 }
