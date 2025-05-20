dbd54ea98fe364128db0de39a3ab16eba72830cf
abhis018 <76279317+abhis018@users.noreply.github.com>
2025-05-20 23:35:02 +0530

    Creating a leader board of freeCodeCamp

---
+++ module-5/fCC-leader-board/leaderBoard.js
@@ -0,0 +1,63 @@
+const forumLatest = "https://cdn.freecodecamp.org/curriculum/forum-latest/latest.json";
+const forumTopicUrl = "https://forum.freecodecamp.org/t/";
+const forumCategoryUrl = "https://forum.freecodecamp.org/c/";
+const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp";
+
+const postsContainer = document.getElementById("posts-container");
+
+const timeAgo = (time) => {
+  const currentTime = new Date();
+  const lastPost = new Date(time);
+  let minutes = Math.floor((currentTime - lastPost) / 60000);
+let hours = Math.floor((currentTime - lastPost) / 3600000);
+let days = Math.floor((currentTime - lastPost) / 86400000);
+  if(minutes < 60){
+    return `${minutes}m ago`;
+  }else if(hours < 24){
+    return `${hours}h ago`;
+  }else {
+    return `${days}d ago`;
+  }
+};
+
+const fetchData = async () => {
+  try {
+    const res = await fetch(forumLatest);
+    const data = await res.json();
+    showLatestPosts(data);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+fetchData();
+
+const showLatestPosts = (data) => {
+  const { topic_list, users } = data;
+  const { topics } = topic_list;
+
+  postsContainer.innerHTML = topics.map((item) => {
+    const {
+      id,
+      title,
+      views,
+      posts_count,
+      slug,
+      posters,
+      category_id,
+      bumped_at,
+    } = item;
+
+    return `
+    <tr>
+      <td>
+        <p class="post-title">${title}</p>
+      </td>
+      <td></td>
+      <td>${posts_count - 1}</td>
+      <td>${views}</td>
+      <td></td>
+    </tr>`;
+  }).join("");
+};
+
